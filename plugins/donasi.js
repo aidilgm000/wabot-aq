9dccd2139ecdf909a1eeadf6a704295fe7bd9d24
aidilgm000 <111693383+aidilgm000@users.noreply.github.com>
2022-09-02 16:15:10 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,12 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [085895086040]
-│ • Telkomsel [081334177340]
+│ • Indosat Ooredoo [081380448281]
+│ • Telkomsel [081380448281]
 ╰────
 
-╭─「 Donasi • Non Pulsa 」
-│ • https://saweria.co/Nurutomo
-│ • https://saweria.co/ariffb
+╭─「 • Non Pulsa 」
+│ • 
+│ • gada
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
